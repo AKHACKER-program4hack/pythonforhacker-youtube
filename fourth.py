d3cb89005c8e288ab59368e0dff73d306f289297
AKHACKER-program4hack <76469481+AKHACKER-program4hack@users.noreply.github.com>
2020-12-23 16:29:27 +0000

    Add files via upload

---
+++ fourth.py
@@ -0,0 +1,17 @@
+list1 = ["egg","vergetable", "hi", "hi", "4", 5.6, 1, True]
+list2 = [1,2,3,4,9,8,7.4]
+        # 0    1             2    3   4    5
+
+# list1[0] = 9.6
+# print(list1.count("egg"))
+
+# list1.append("milk")
+# list1.clear()
+# list1.pop()
+# list1.remove("hi")
+# list1.reverse()
+# list2.sort()
+
+# print(list2)
+
+print(list1[0::-2])
